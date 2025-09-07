095c1e015e7c093be4c0c76c08b373dff85fb12f
Manikanta11-mad <manikanta00645@gmail.com>
2025-09-07 21:40:34 +0530

    admin is

--- student/src/App.js
+++ student/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Home from './Student/home';
-import About from './Student/about'; // Capitalized import
 import Contact from './Student/contact';
 import Courses from './Student/courses';
 import Teachers from './Student/teachers';
@@ -11,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+
         <Route path="/home" element={<Home />} /> 
         <Route path='/contact' element={<Contact />} />
         <Route path='/courses' element={<Courses />} />
